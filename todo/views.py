335dee35f8b4d8cda305ebf20f1e98ca872c9d78
Snowmaru0283 <67153646+Snowmaru0283@users.noreply.github.com>
2022-06-13 13:11:59 +0200

    removed unecessary requirements

--- todo/views.py
+++ todo/views.py
@@ -4,8 +4,9 @@ from .models import *
 from .forms import *
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
 
 
 @login_required(login_url='login')
